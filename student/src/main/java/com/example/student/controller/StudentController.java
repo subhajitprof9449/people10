1c9a24ec73c4a5a747caf3a97224440f5c16c15c
Subhajit Mishra <subhajitmishra@integramicro.co.in>
2020-07-18 23:05:41 +0530

    Adding ResponseBody

--- student/src/main/java/com/example/student/controller/StudentController.java
+++ student/src/main/java/com/example/student/controller/StudentController.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.student.model.Student;
-import com.example.student.model.StudentWrapper;
 import com.example.student.service.StudentService;
 import com.example.student.util.StudentUtil;
 
@@ -19,22 +19,19 @@ public class StudentController {
 
 	@Autowired
 	private StudentService studSer;
-	
+
 	@Autowired
 	private StudentUtil studUtil;
 
-	public StudentWrapper students;
-	
 	@RequestMapping("/getStudentReport")
-	public StudentWrapper getStudentReport() throws JRException {
+	public @ResponseBody List<Student> getStudentReport() throws JRException {
 		List<Student> studentListForReport = new ArrayList();
 		studentListForReport = studSer.getStudentList();
-		students= new StudentWrapper(studentListForReport);
 		studUtil.getReport(studentListForReport);
-		return students;
-		
+		return studentListForReport;
+
 	}
-	
+
 	@RequestMapping("/test")
 	public String test() {
 		return "Test Measage";
